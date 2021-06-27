06814648b66be49b408982b45789126757d1d43f
zhikun <wangzhikun0807@qq.com>
2021-06-27 23:19:49 +0800

    第一次提交

---
+++ src/main/java/com/learn/GitTest.java
@@ -0,0 +1,12 @@
+package com.learn;
+
+/**
+ * @Author：wangzhikun
+ * @Date：2021/6/27 23:18
+ * @Desc：
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git-demo");
+    }
+}
